71223e706287f8b59abfca778ba1514881f63880
Florian Stellbrink <florian@stellbr.ink>
2018-10-29 17:33:57 +0100

    Fix prediction class to show top n for next word instead of n next words

--- prediction.py
+++ prediction.py
@@ -10,14 +10,13 @@
 
 # Do not use GPU for testing (it is probably busy training)
 from os import environ
+print("Disabling GPU")
+environ['CUDA_VISIBLE_DEVICES'] = '-1'
 from pathlib import Path
 
 from keras import Model
 from keras.engine.saving import load_model
-from numpy import argmax, array
-
-print("Disabling GPU")
-environ['CUDA_VISIBLE_DEVICES'] = '-1'
+from numpy import argmax, array, argsort
 
 from keras_preprocessing.sequence import pad_sequences
 from sklearn.neighbors import NearestNeighbors
@@ -52,6 +51,7 @@ class PredictiveSearch:
         self.tokenizer_length = len(self.tokenizer.word_index)
         self.max_caption_size = 249
         self.word_count = 5
+        self.sentence_count = 5
 
         # Generate encoded representations for all images
         print("Loading images...")
@@ -61,6 +61,39 @@ class PredictiveSearch:
         self.images = self.images.reshape((self.images.shape[0], -1))
         print("Initializing nearest neighbor search...")
         self.nearest_neighbors = NearestNeighbors(n_neighbors=3).fit(self.images)
+
+    def show_images(self, words: str):
+        """
+        Show the three images that correspond most with the input.
+        :param words: Input sentence to find images for
+        """
+        tokens = self.tokenizer.texts_to_sequences([words])[0]
+        tokens = pad_sequences([tokens], maxlen=self.max_caption_size, padding='pre')
+        image = self.model.predict(tokens, verbose=0)[1]
+        _, indices = self.nearest_neighbors.kneighbors(image.reshape((1, -1)))
+        image_ids = self.image_ids[indices[0]]
+        for image in mycoco.get_images(image_ids):
+            plt.imshow(image)
+
+    def sentence_prediction(self, words: str):
+        """
+        This is not part of the assignment, I just liked the idea.
+        Given an initial sentence, repeatedly predict the next word, effectively extending the sentence.
+        Show the most likely pictures before and after sentence prediction.
+        :param words: Initial words to predict sentence from
+        """
+        print(f"Input sentence: {words}")
+        self.show_images(words)
+
+        for _ in range(self.sentence_count):
+            tokens = self.tokenizer.texts_to_sequences([words])[0]
+            tokens = pad_sequences([tokens], maxlen=self.max_caption_size, padding='pre')
+            predicted_class = argmax(self.model.predict(tokens, verbose=0)[0])
+            predicted_word = self.tokenizer.index_word[predicted_class]
+            words += " " + predicted_word
+
+        print(f"Predicted sentence: {words}")
+        self.show_images(words)
 
     def predictive_search(self, words: str):
         """
@@ -84,26 +117,20 @@ class PredictiveSearch:
             if word not in self.tokenizer.word_index:
                 print(f"Word not in training set: {word}")
 
-        # 3. Predict five additional words, keep track of latest predicted image
-        image = None
-        for _ in range(self.word_count):
-            tokens = self.tokenizer.texts_to_sequences([words])[0]
-            tokens = pad_sequences([tokens], maxlen=self.max_caption_size, padding='pre')
-            predicted = self.model.predict(tokens, verbose=0)
-            predicted_class = argmax(predicted[0])
-            predicted_word = self.tokenizer.index_word[predicted_class]
-            words += " " + predicted_word
-            image = predicted[1]
-        print(f"Predicted sentence: {words}")
+        # 3. Predict five most likely next words
+        tokens = self.tokenizer.texts_to_sequences([words])[0]
+        tokens = pad_sequences([tokens], maxlen=self.max_caption_size, padding='pre')
+        predicted = self.model.predict(tokens, verbose=0)
+        predicted_classes = argsort(predicted[0][0])[::-1][:5]
+        predicted_words = list(map(lambda p_class: self.tokenizer.index_word[p_class], predicted_classes))
+        print(f"Input sentence: {words}")
+        print(f"Most likely next words: {predicted_words}")
 
         # 4. Find three nearest neighbors of all images
-        _, indices = self.nearest_neighbors.kneighbors(image.reshape((1, -1)))
-        image_ids = self.image_ids[indices[0]]
-        for image in mycoco.get_images(image_ids):
-            plt.imshow(image)
+        self.show_images(words)
 
 
 if __name__ == "__main__":
     ps = PredictiveSearch()
     ps.predictive_search("A toilet standing")
-    ps.predictive_search("the Man is")
+    ps.sentence_prediction("The man is")
